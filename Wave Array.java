b867789d99b0d26c047e4bcfe8df937c919d7f98
Haiderali43707 <105425518+Haiderali43707@users.noreply.github.com>
2022-07-03 06:52:32 +0530

    Create Wave Array.java

---
+++ Wave Array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public static void convertToWave(int n, int[] a) {
+        // code here
+        int temp=0;
+        for(int i=0;i<n-1;i++)
+        {
+            temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+            i++;
+        }
+        
+    }
+}
+        
